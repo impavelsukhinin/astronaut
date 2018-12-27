fef7dc46c7ede81365915784aedc5d0f4a2f763a
impavelsukhinin <p.sukhinin@rocketguys.com>
2018-12-27 11:31:19 +0300

    initial starter pack

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import styles from './App.pcss'
+import picture from './assets/img.jpg'
+
+import { AppProps } from './App.d'
+
+const App = ({ title = 'React Typescript Starter Pack' }: AppProps) => (
+	<div className={styles.root}>
+		<img className={styles.picture} src={picture} alt="img"/>
+		<h1 className={styles.title}>{ title }</h1>
+	</div>
+)
+
+export default App
